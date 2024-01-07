126fa2ad7d678e8822085008e2ba03e5d5b98371
jjablonski <111350294+Jaanuszek@users.noreply.github.com>
2024-01-07 15:33:26 +0100

    Update Air_Quality_Meter.ino

--- Air_Quality_Meter.ino
+++ Air_Quality_Meter.ino
@@ -21,33 +21,10 @@ struct pms5003data {
 struct pms5003data data;
     
 void loop() {
-  //if(!Serial.available()){
-  //  return 0;
-  //}
   if (readPMSdata(&pmsSerial)&& data.pm10_env <400 && data.pm25_env <400 && data.pm100_env <400) {
-    // reading data was successful!
-    //Serial.println();
-    //Serial.println("---------------------------------------");
-    //Serial.println("Concentration Units (standard)");
-    //Serial.print("PM 1.0: "); Serial.print(data.pm10_standard);
-    //Serial.print("\t\tPM 2.5: "); Serial.print(data.pm25_standard);
-    //Serial.print("\t\tPM 10: "); Serial.println(data.pm100_standard);
-    //Serial.println("---------------------------------------");
-    //Serial.println("Concentration Units (environmental)");
-   // Serial.print("PM 1.0: "); Serial.print(data.pm10_env);
-    //Serial.print("PM 2.5: "); Serial.print(data.pm25_env);
-    //Serial.print("PM 10: "); Serial.println(data.pm100_env);
     Serial.print(data.pm10_env); Serial.print(" ");
     Serial.print(data.pm25_env); Serial.print(" ");
     Serial.println(data.pm100_env);
-    //Serial.println("---------------------------------------");
-    //Serial.print("Particles > 0.3um / 0.1L air:"); Serial.println(data.particles_03um);
-    //Serial.print("Particles > 0.5um / 0.1L air:"); Serial.println(data.particles_05um);
-    //Serial.print("Particles > 1.0um / 0.1L air:"); Serial.println(data.particles_10um);
-    //Serial.print("Particles > 2.5um / 0.1L air:"); Serial.println(data.particles_25um);
-    //Serial.print("Particles > 5.0um / 0.1L air:"); Serial.println(data.particles_50um);
-    //Serial.print("Particles > 10.0 um / 0.1L air:"); Serial.println(data.particles_100um);
-    //Serial.println("---------------------------------------");
     delay(1000);
   }
 }
@@ -76,14 +53,7 @@ boolean readPMSdata(Stream *s) {
   for (uint8_t i=0; i<30; i++) {
     sum += buffer[i];
   }
- 
-  /* debugging
-  for (uint8_t i=2; i<32; i++) {
-    Serial.print("0x"); Serial.print(buffer[i], HEX); Serial.print(", ");
-  }
-  Serial.println();
-  */
-  
+
   // The data comes in endian'd, this solves it so it works on all platforms
   uint16_t buffer_u16[15];
   for (uint8_t i=0; i<15; i++) {
@@ -91,13 +61,8 @@ boolean readPMSdata(Stream *s) {
     buffer_u16[i] += (buffer[2 + i*2] << 8);
   }
  
-  // put it into a nice struct :)
+  // put it into a struct
   memcpy((void *)&data, (void *)buffer_u16, 30);
  
-  //if (sum != data.checksum) {
-  //  Serial.println("Checksum failure");
-  //  return false;
-  //}
-  // success!
   return true;
 }
